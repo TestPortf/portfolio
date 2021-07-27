ee5f756ce35a1d3fd555a730d14d2b2dd2f5310a
TestPortf <lsakjfmslejr@riseup.net>
2021-07-27 07:47:10 +0000

    ldsjkf

--- diplom/text_gen.py
+++
@@ -1,3 +0,0 @@
-def if __name__ == "__main__":
-    for element in range(0, 12):
-        print(f'button_state[{element}] >>= 1')
--- filters/kurs/main.c
+++ filters/kurs/main.c
@@ -65,7 +65,7 @@ unsigned int filter_signal(double A[5], double B[5], unsigned int current_point,
 void main (void)
 {
 	unsigned int current_adc_data;
-	unsigned int past_adc_data = 1;
+	unsigned int dac_data;
 	double A_coef[5] = {1, -3.8261594180878564230852134642191231250762939453125,
 											5.64021677662502707306657612207345664501190185546875,
 											-3.788568424693382841184075005003251135349273681640625,
@@ -73,11 +73,29 @@ void main (void)
 	double B_coef[5] = {0.0000482615212977725109556002835997645661336719058454036712646484375,
 											0, -0.000096523042595545021911200567199529132267343811690807342529296875,
 											0, 0.0000482615212977725109556002835997645661336719058454036712646484375};
+    double xk_4 = 0, xk_3 = 0, xk_2 = 0, xk_1 = 0, xk = 0, yk_4 = 0, yk_3 = 0, yk_2 = 0, yk_1 = 0, yk = 0;
+
 	
 	bit_12_8 = 1; CS = 0; A0 = 0; CE = 1;
 	
 	while(1){
 		current_adc_data = ad1674_read() >> 2;
-		shift_transmit(current_adc_data);
+
+        xk_4 = xk_3
+        xk_3 = xk_2
+        xk_2 = xk_1
+        xk_1 = xk
+
+        xk = current_adc_data
+
+        yk_4 = yk_3
+        yk_3 = yk_2
+        yk_2 = yk_1
+        yk_1 = yk
+        yk = B_coef[0] * xk + B_coef[2] * xk_2 + B_coef[4] * xk_4 - A_coef[1] * yk_1 - A_coef[2] * yk_2 - A_coef[3] * yk_3 - A_coef[4] * yk_4
+
+        dac_data = yk;
+
+		shift_transmit(dac_data);
 	}
 }
